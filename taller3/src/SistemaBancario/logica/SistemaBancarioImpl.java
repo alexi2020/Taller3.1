ca51382b5bb2a20ce772b73cdeb42c933ea7a2b9
Aburr <Aburr@DESKTOP-T0O29L7>
2020-07-21 13:07:49 -0400

    sistema bancario impl

--- taller3/src/SistemaBancario/logica/SistemaBancarioImpl.java
+++ taller3/src/SistemaBancario/logica/SistemaBancarioImpl.java
@@ -7,20 +7,33 @@ public class SistemaBancarioImpl implements SistemaBancario {
 	private ListaCuenta listaCuentas;
 	public SistemaBancarioImpl() {
 		listaPersonas=new ListaPersona(100);
-		listaCuentas=new ListaCuenta(2);
-	//	
+		listaCuentas=new ListaCuenta(100);
+	//hola	dasdaasddasds
 	}
 	public boolean ingresarPersona(String rut,String nombre,String apellido,String contraseña, int matriz[][]) {
 		Persona persona=new Persona(rut,nombre,apellido,contraseña,matriz);
 		return listaPersonas.IngresarPersona(persona);
 	}
 	public boolean ingresarCuentaCorriente(String numeroCuenta,String rutTitular,String contraseña,long saldo) {
-		Cuenta cuentaC=new CuentaCorriente(numeroCuenta,rutTitular,contraseña,saldo);
-		return listaCuentas.IngresarCuenta(cuentaC);
+		Cuenta cuenta = listaCuentas.BuscarCuenta(numeroCuenta);
+		if(cuenta == null) {
+			Cuenta cuentaC=new CuentaCorriente(numeroCuenta,rutTitular,contraseña,saldo);
+			return listaCuentas.IngresarCuenta(cuentaC);
+		}
+		else{
+			throw new NullPointerException("Posible Hackeo");
+		}
 	}
 	public boolean ingresarCuentaChequeraElectronica(String numeroCuenta,String rutTitular,String contraseña,long saldo) {
-		Cuenta cuentaChequeraE=new CuentaChequeraElectronica(numeroCuenta,rutTitular,contraseña,saldo);
-		return listaCuentas.IngresarCuenta(cuentaChequeraE);
+		Cuenta cuenta = listaCuentas.BuscarCuenta(numeroCuenta);
+		if(cuenta == null) {
+			Cuenta cuentaChequeraE=new CuentaChequeraElectronica(numeroCuenta,rutTitular,contraseña,saldo);
+			return listaCuentas.IngresarCuenta(cuentaChequeraE);
+		}
+		else {
+			throw new NullPointerException("Posible Hackeo");
+		}
+		
 	}
 	public boolean verificarCuentaInicioSesion(String rut ,String contraseñaInicio) {
 		Persona persona=listaPersonas.BuscarPersona(rut);
